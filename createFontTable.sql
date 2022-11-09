f757b939e9fa75f6cb838401a58985a1a756e74e
Tori <tori.thompson@student.sl.on.ca>
2022-11-09 13:27:19 -0500

    new file: createFontTable.sql

---
+++ createFontTable.sql
@@ -0,0 +1,7 @@
+Use comp220db;
+create table fontNames (fontName varchar(50), primary key (fontName));
+insert into fontNames values ('Arial');
+insert into fontNames values ('Brush Script MT');
+insert into fontNames values ('Comic Sans MS');
+insert into fontNames values ('Courier New');
+insert into fontNames values ('Verdana');
