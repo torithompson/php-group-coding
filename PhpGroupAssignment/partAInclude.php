67057bdca77b4650bd4fb1e3c44703f4f74003a4
NEenemee <113130041+NEenemee@users.noreply.github.com>
2022-11-10 13:43:58 -0500

    Add files via upload

---
+++ PhpGroupAssignment/partAInclude.php
@@ -0,0 +1,79 @@
+<?php
+function WriteHeaders($Heading="Welcome",$TitleBar="MySite")
+{
+    echo "
+        <!doctype html>
+        <html lang = \"en\">
+        <head>
+            <meta charset = \"UTF-8\">
+            <link rel =\"stylesheet\" type = \"text/css\" href=\"Styles.css\"/>
+        </head>
+        <title> $TitleBar </title>\n
+        <body>\n
+        <h1>$Heading</h1>\n
+        <h2>By: Nick, Abdul, Tori, Trevor, Anuj, Nelson</h2>
+    ";
+}
+function DisplayLabel($passLabel)
+{
+    echo "<label>$passLabel</label>";
+}
+function DisplayTextbox($type,$name,$size,$value = 0)
+{
+    echo "
+    <input type = $type name = $name Size = $size value = $value >
+    ";
+}
+function DisplayImage($fileName,$alt,$height,$width)
+{
+    echo "<img src = $fileName height=$height width=$width alt=$alt/>";
+}
+function DisplayButton($name,$text,$fileName = "",$alt="")
+{
+    echo "<button type = submit name=$name>$text<img src = $fileName alt=$alt></button>";
+}
+function displayContactInfo()
+{
+    echo "Questions? Comments? ";
+    echo "<a href=\"mailto:nick.eliopoulos@student.sl.on.ca\">
+            Nick.Eliopoulos@student.sl.on.ca</a></p>";
+
+}
+function WriteFooters()
+{  
+    echo "<footer>";
+    displayContactInfo();
+    echo "</footer>";
+    echo "</body>\n";
+    echo "</html>\n";
+}
+function CreateConnectionObject()
+{
+    $fh = fopen('auth.txt','r');
+    $Host =  trim(fgets($fh));
+    $UserName = trim(fgets($fh));
+    $Password = trim(fgets($fh));
+    $Database = trim(fgets($fh));
+    $Port = trim(fgets($fh)); 
+    fclose($fh);
+    $mysqlObj = new mysqli($Host, $UserName, $Password,$Database,$Port);
+    // if the connection and authentication are successful, 
+    // the error number is 0
+    // connect_errno is a public attribute of the mysqli class.
+    if ($mysqlObj->connect_errno != 0) 
+    {
+     echo "<p>Connection failed. Unable to open database $Database. Error: "
+              . $mysqlObj->connect_error . "</p>";
+     // stop executing the php script
+     exit;
+    }
+    echo "<form action = http://localhost/EliopoulosNickCodingAsst/asstMain.php 
+        method = post>";
+    echo "<form>";
+    return ($mysqlObj);
+
+}
+
+
+// main
+?>
