bc1f267868cb0b18e3e59150d643f9150ad1ccd4
Tori <tori.thompson@student.sl.on.ca>
2022-11-16 13:10:41 -0500

    Add files via upload

---
+++ final/websiteScript.js
@@ -0,0 +1,9 @@
+function changeFont(fontChoice)
+{ 
+	document.getElementById("textArea").style.fontFamily = fontChoice;
+
+}
+function changeSize(sizeChoice)
+{ 
+	document.getElementById("textArea").style.fontSize = sizeChoice;
+}
