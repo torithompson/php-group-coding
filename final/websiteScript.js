822bca71a6127dce2c1dd2365252582859da3ba5
Tori <tori.thompson@student.sl.on.ca>
2022-11-17 12:41:38 -0500

    Delete final directory

--- final/websiteScript.js
+++
@@ -1,9 +0,0 @@
-function changeFont(fontChoice)
-{ 
-	document.getElementById("textArea").style.fontFamily = fontChoice;
-
-}
-function changeSize(sizeChoice)
-{ 
-	document.getElementById("textArea").style.fontSize = sizeChoice;
-}
